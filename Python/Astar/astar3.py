d2970e134575cdaf6f098e2630a0575428538522
evcys <evchua45@gmail.com>
2024-02-02 00:21:06 +0800

    testing with astar

---
+++ Python/Astar/astar3.py
@@ -0,0 +1,121 @@
+class Node():
+    def __init__(self, parent=None, position=None, direction=None):
+        self.parent = parent
+        self.position = position
+        self.direction = direction  # Added direction attribute
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    open_list = []
+    closed_list = []
+    open_list.append(start_node)
+
+    while len(open_list) > 0:
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append((current.position, current.direction))
+                current = current.parent
+            return path[::-1]
+
+        children = []
+        for new_position in [(0, -1, 'W'), (0, 1, 'E'), (-1, 0, 'N'), (1, 0, 'S')]:
+            node_position = (
+                current_node.position[0] + new_position[0],
+                current_node.position[1] + new_position[1],
+            )
+
+            if (
+                node_position[0] > (len(maze) - 1)
+                or node_position[0] < 0
+                or node_position[1] > (len(maze[len(maze) - 1]) - 1)
+                or node_position[1] < 0
+            ):
+                continue
+
+            if maze[node_position[0]][node_position[1]] != 0:
+                if new_position[2] != maze[node_position[0]][node_position[1]]:
+                    continue
+
+            new_node = Node(current_node, node_position, new_position[2])
+            children.append(new_node)
+
+        for child in children:
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + (
+                (child.position[1] - end_node.position[1]) ** 2
+            )
+            child.f = child.g + child.h
+
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            open_list.append(child)
+
+
+def visualize_path(maze, path):
+    for row in range(len(maze)):
+        for col in range(len(maze[row])):
+            if (row, col) == path[0][0]:
+                print("S", end=" ")  # Start
+            elif (row, col) == path[-1][0]:
+                print("E", end=" ")  # End
+            elif (row, col) in [p[0] for p in path]:
+                print("*", end=" ")  # Path
+            elif maze[row][col] in ["N", "S", "W", "E"]:
+                print(maze[row][col], end=" ")  # Obstacle direction
+            else:
+                print(".", end=" ")  # Open space
+        print()
+
+
+def main():
+    maze = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, "W", 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, "N", 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, "W", 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+
+    start = (0, 0)
+    end = (7, 6)
+
+    path = astar(maze, start, end)
+    visualize_path(maze, path)
+
+
+if __name__ == "__main__":
+    main()
