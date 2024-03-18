cb66bad9e8a9ec91ebd262d74a5ffb3e9dc9f701
evcys <evchua45@gmail.com>
2024-03-18 15:33:06 +0800

    test

--- Python/Task2/task2.py
+++ Python/Task2/task2.py
@@ -110,152 +110,183 @@ def main_loop():
 		"""first image recognition = 'left"""
 		'S2000', # move 20cm backwards
 		'A4045', # Turn left 40cm, 45 degree
-		'D4060', # Turn Right 40cm, 60 degree
-		'A3045', # Turn Left 30cm, 45 degree
-		# 'FL057', 'FR057',
+		'D4045', # Turn Right 40cm, 60 degree
 
-		"""move towards to 2nd obstacle"""
-		'W3000', # Move forward, towards 2nd obstacle
-		# 'US030',  # move until 30cm before 2nd obstacle and save d to buff 0
-		# 'FR020',
+		# """move towards to 2nd obstacle"""
+		# 'W5000', # Move forward, towards 2nd obstacle
 	]
 
 	case_r = [
 		"""first image recognition = 'right'"""
 		'S2000', # move 20cm backwards
 		'D4045', # Turn Right 40cm, 45 degree
-		'A4060', # Turn Left 40cm, 60 degree
-		'D3045', # Turn Right 30cm, 45 degree
-		# 'FR057', 'FL057',
+		'A4045', # Turn Left 40cm, 60 degree
+		# 'D3045', # Turn Right 30cm, 45 degree
 
-		"""move over to 2nd obstacle"""
-		'W3000', # Move forward, towards 2nd obstacle
-		# 'US030',  # move until 30cm before 2nd obstacle and save d to buff 0
-		# 'FL020',
+		# """move over to 2nd ob stacle"""
+		# 'W5000', # Move forward, towards 2nd obstacle
 	]
+ 
 
-	"""Second obstacle will be 60cm by 10cm"""
-	case_ll = [ # 1st image: left, 2nd image: left
+	"""Second obstacle will be unknown"""
+	case_2l = [
 		"""second image recognition = 'left'"""
-		'S2000', # move 20cm backwards
-		'A4090', # Turn left 40cm, 90 degree
-		'D2090', # Turn right 20cm, 90 degree
-		# roundabout the second obstacle
-		# 'D7090', # Turn right 70cm, 90 degree
-		'D1090', # Turn right 10cm, 90 degree
-		'W7000', # Go straight for 70cm
-		'D1090', # Turn right 10cm, 90 degree
+  		# roundabout the second obstacle
+		'T2000'
+		'D9090',
+		'W5000',
+		'D9090',
 
 		"""back to base (carpark)"""
-		'W',
-		# 'US120', # move until 20cm before parking wall
-		# 'US120'  # move until 20cm before parking wall
 	]
-
-	case_lr = [ # 1st image: left, 2nd image: right
+ 
+	case_2r = [
 		"""second image recognition = 'right'"""
-  		'S2000', # move 20cm backwards
-		'', # Turn right
-		'W2000', # Go straight 20cm
-		'A3090', # Turn left 30cm, 180 degree
-		'A3090', 
-		# can turn A3000?
-		'W6000', # Go straight
-		'A3090', # Turn left 30cm, 90 degree
+  		# roundabout the second obstacle
+		'T2000'
+		'A9090',
+		'W5000',
+		'A9090',
+		
 
 		"""back to base (carpark)"""
-		# 'US120', # move until 20cm before parking wall
-		# 'US120'  # move until 20cm before parking wall
 	]
-
-	case_rr = [ # 1st image: right, 2nd image: right
-		"""second image recognition = 'right'"""
-		# 'BL020', 'FR067', 'FL067', 'FW025', 'FL090', 'FW060', 'FL090',
-		# 'RT070',  # d + 70
-		# 'FL090', 'FR090',
-
-		"""back to base (carpark)"""
-		# 'US120', # move until 20cm before parking wall
-		# 'US120'  # move until 20cm before parking wall
-	]
-
-	case_rl = [ # # 1st image: right, 2nd image: left
-		"""second image recognition = 'left'"""
-		# 'FL070', 'FW025', 'FR090', 'FW025', 'FR090', 'FW060', 'FR090',
-		# 'RT075',  # d + 75
-		# 'FR090', 'FL090',
-
-		"""back to base (carpark)"""
-		# 'US120', # move until 20cm before parking wall
-		# 'US120'  # move until 20cm before parking wall
-	]
-
-	# instr_list = []
-	init = ['W5000'] # move 50cm forward  
-	# will stop 10cm before obstacle
 	
+	# case_ll = [ # 1st image: left, 2nd image: left
+    #     """second image recognition = 'left'"""
 
+	# 	# roundabout the second obstacle
+		
+
+	# 	"""back to base (carpark)"""
+		
+	# 	# """second image recognition = 'left'"""
+	# 	# # 'S2000', # move 20cm backwards
+	# 	# 'A4045', # Turn left 45 degree
+	# 	# 'W8000',
+  	# 	# 'W8000',	
+	# 	# 'D2090', # Turn right 20cm, 90 degree
+	# 	# # roundabout the second obstacle
+	# 	# 'W8000',
+	# 	# 'D4090',
+	# 	# # 'D7090', # Turn right 70cm, 90 degree
+	# 	# # 'D1090', # Turn right 10cm, 90 degree
+	# 	# # 'W7000', # Go straight for 70cm
+	# 	# # 'D1090', # Turn right 10cm, 90 degree
+
+	# 	# """back to base (carpark)"""
+	# 	# 'W8000',
+  	# 	# 'W8000',
+	# ]
+
+	# case_lr = [ # 1st image: left, 2nd image: right
+	# 	"""second image recognition = 'right'"""
+  	# 	'S2000', # move 20cm backwards
+	# 	'', # Turn right
+	# 	'W2000', # Go straight 20cm
+	# 	'A3090', # Turn left 30cm, 180 degree
+	# 	'A3090', 
+	# 	# can turn A3000?
+	# 	'W6000', # Go straight
+	# 	'A3090', # Turn left 30cm, 90 degree
+
+	# 	"""back to base (carpark)"""
+	# ]
+
+	# case_rr = [ # 1st image: right, 2nd image: right
+	# 	"""second image recognition = 'right'"""
+		
+
+	# 	"""back to base (carpark)"""
+	# ]
+
+	# case_rl = [ # # 1st image: right, 2nd image: left
+	# 	"""second image recognition = 'left'"""
+
+	# 	"""back to base (carpark)"""
+	# ]
+
+	# init = ['W9000']  # move 90cm forward
+	# will stop 30cm before obstacle?
+ 
+	# Keep track of the forward distance from the carpark to the first obstacle
+	hcounter1 = 0 
+	hcounter2 = 0
+	vcounter2 = 0
+	init = ['W1000']
+	
+	# idea
+ 	# measure the distance from carpark to first obstacle in 10s
+	while (): #(receive from stm)
+		perform_stm_write(init[0])
+		hcounter1+=1
+	
 	"""CHANGE ACCORDINGLY, TESTING"""
 	# img1 = "left_arrow"
 	# img2 = "left_arrow"
 
-	perform_stm_write(init[0])
-	
-	# image = img_capture()
-	# img1 = process_img(image)
 	img1 = perform_classification()
 	print(f'image1:{img1}')
  
 	"""continue moving forward if there is no image detected"""
-	while img1 == "":
-		perform_stm_write(init[0])
-		img1 = perform_classification()
-		print(f'image1:{img1}')
-  
+	# while img1 == "":
+	# 	perform_stm_write(init[0])
+	# 	img1 = perform_classification()
+	# 	print(f'image1:{img1}')
 
 	if "right_arrow" in img1:
-		# move_stm(case_r)
 		for instr in case_r:
 			print(f'instruction:{instr}')
 			perform_stm_write(instr=instr)
 		# time.sleep(10)
-		# image2 = img_capture()
-		# img2 = process_img(image2)
-		img2 =  perform_classification()
-		print(f'image2:{img2}')
+		# img2 =  perform_classification()
+		# print(f'image2:{img2}')
 
 	elif "left_arrow" in img1:
-		# move_stm(case_l)
 		for instr in case_l:
 			print(f'instruction:{instr}')
 			perform_stm_write(instr=instr)
 		# time.sleep(10)
-		# image2 = img_capture()
-		# img2 = process_img(image2)
-		img2 =  perform_classification()
-		print(f'got img2:{img2}')
-
+		# img2 =  perform_classification()
+		# print(f'got img2:{img2}')
+	
+	# measure the distance from carpark to second obstacle in 10s
+	while (): #(receive from stm)
+		perform_stm_write(init[0])
+		hcounter2+=1
+  
+	img2 =  perform_classification()
+	print(f'got img2:{img2}')
 
 	# Checking if the first image is left and the second image is left.
-	if "left_arrow" in img1 and "left_arrow" in img2:
-		for instr in case_ll:
+	if "left_arrow" in img2:
+		# turn left first
+		perform_stm_write('A9090')
+		while (): #(receive from stm)
+				perform_stm_write(init[0])
+				vcounter2+=1
+		for instr in case_2l:
 			perform_stm_write(instr)
+
 
 	# Checking if the first image is left and the second image is right.
-	if "left_arrow" in img1 and "right_arrow" in img2:
-		for instr in case_lr:
+	if "right_arrow" in img2:
+		# turn right first
+		perform_stm_write('A9090')
+		while (): #(receive from stm)
+				perform_stm_write(init[0])
+				vcounter2+=1
+		for instr in case_2r:
 			perform_stm_write(instr)
 
-	# Checking if the first image is right and the second image is left.
-	if "right_arrow" in img1 and "left_arrow" in img2:
-		for instr in case_rl:
-			perform_stm_write(instr)
+	# # Checking if the first image is right and the second image is left.
+	# if "right_arrow" in img1 and "left_arrow" in img2:
+	# 	for instr in case_rl:
+	# 		perform_stm_write(instr)
 
-	# Checking if the first image is right and the second image is right.
-	if "right_arrow" in img1 and "right_arrow" in img2:
-		for instr in case_rr:
-			perform_stm_write(instr)
-
-    
+	# # Checking if the first image is right and the second image is right.
+	# if "right_arrow" in img1 and "right_arrow" in img2:
+	# 	for instr in case_rr:
+	# 		perform_stm_write(instr)
 
 main_loop()
